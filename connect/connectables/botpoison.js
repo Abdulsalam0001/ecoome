54627dacbf792476a90e0041a64a4a518a0d1d09
chris chase <dreshaw2012@gmail.com>
2023-02-28 17:27:29 +0000

    hvhj

--- connect/connectables/botpoison.js
+++
@@ -1,7 +0,0 @@
-import Botpoison from "@botpoison/browser";
-          
-const botpoison = new Botpoison({
-  publicKey: "pk_11962bc3-48db-4281-bb83-aa411cd4588c"
-});
-
-const { solution } = await botpoison.challenge();
